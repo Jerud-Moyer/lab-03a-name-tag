6f344936a4a382a345923ba8b1a28cdee6259e6d
Jerud-Moyer <jerudmoyer@gmail.com>
2020-06-24 16:30:29 -0700

    one step closer

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const myButton = document.getElementById('nameButton')
 myButton.addEventListener('click', () => {
     console.log('it worked!');
 });
+
+const userName = document.getElementById('nameField');
+userName.addEventListener('input', () => {
+    console.log("WOW!");
+});
