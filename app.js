4c6c81bb6aad2ccb64d43a404e75c60ab81c5089
Jerud-Moyer <jerudmoyer@gmail.com>
2020-06-24 17:16:03 -0700

    name functional

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const myButton = document.getElementById('nameButton')
+const myButton = document.getElementById('nameButton');
 myButton.addEventListener('click', () => {
+    const userNameInput = document.getElementById('nameField');
+    const nameDisplay = document.getElementById('name');
+   const value = userNameInput.value;
+   nameDisplay.textContent = value;
+    
+    //get input
+    //get inputs value
+    //get div
+    // set text content div to input value
     console.log('it worked!');
 });
 
-const userName = document.getElementById('nameField');
-userName.addEventListener('input', () => {
-    console.log("WOW!");
-});
