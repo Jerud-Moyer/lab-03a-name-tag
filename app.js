21c1ccc3fc17a5bf3b2457a2fa58ec4000728fc9
Jerud-Moyer <jerudmoyer@gmail.com>
2020-06-25 09:25:47 -0700

    cleaning up

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ myButton.addEventListener('click', () => {
 });
 
 const greenButton = document.getElementById('buttGreen');
-greenButton.addEventListener('click',() => {
-const colorFieldA = document.getElementById('colorsBottom');
-const colorFieldB = document.getElementById('colorsTop');
-colorFieldA.style.background = 'green';
-colorFieldB.style.background = 'green';
+greenButton.addEventListener('click', () => {
+    const colorFieldA = document.getElementById('colorsBottom');
+    const colorFieldB = document.getElementById('colorsTop');
+    colorFieldA.style.background = 'green';
+    colorFieldB.style.background = 'green';
 
 });
 
 const blueButton = document.getElementById('buttBlue');
-blueButton.addEventListener('click',() => {
+blueButton.addEventListener('click', () => {
     const colorFieldA = document.getElementById('colorsBottom');
     const colorFieldB = document.getElementById('colorsTop');
     colorFieldA.style.background = 'blue';
@@ -29,7 +29,7 @@ blueButton.addEventListener('click',() => {
 });
 
 const pinkButton = document.getElementById('buttPink');
-pinkButton.addEventListener('click',() => {
+pinkButton.addEventListener('click', () => {
     const colorFieldA = document.getElementById('colorsTop');
     const colorFieldB = document.getElementById('colorsBottom');
     colorFieldA.style.background = 'pink';
@@ -37,7 +37,7 @@ pinkButton.addEventListener('click',() => {
 });
 
 const redButton = document.getElementById('buttRed');
-redButton.addEventListener("click", () => {
+redButton.addEventListener('click', () => {
     const colorFieldA = document.getElementById('colorsTop');
     const colorFieldB = document.getElementById('colorsBottom');
     colorFieldA.style.background = 'red';
